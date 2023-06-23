c14cc534d9bb36b200742f5b44d3ac2c8df8dbac
Luis Andrade <luisfer220900@gmail.com>
2023-06-22 21:00:38 -0400

    añadir formulario crear personal

--- agregar_personal.php
+++ agregar_personal.php
@@ -4,6 +4,24 @@ include "./sidebar.php";
 include "./config/conexion.php"
 ?>
 
+<?php
+try {
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = (isset($_POST['id'])) ? $_POST['id'] : "";
+    $nombre = (isset($_POST['nombre'])) ? $_POST['nombre'] : "";
+    $cargo = (isset($_POST['cargo'])) ? $_POST['cargo'] : "";
+
+    $sentenciaSQL = $conexion->prepare("INSERT INTO personal(personal_id, nombre, cargo, disponible) VALUES ('$id', '$nombre', '$cargo', 'disponible');");
+    $sentenciaSQL->execute();
+
+    header("Location: personal.php");
+    exit;
+  }
+} catch (Exception $ex) {
+  echo $ex->getMessage();
+}
+?>
+
 <form method="post" enctype="multipart/form-data" class="text-center">
   <div class="col-md-5 mx-auto">
     <div class="card-formulario" style="height: 300px;">
@@ -15,27 +33,23 @@ include "./config/conexion.php"
           <div class="form-group  ">
             <div class="form-group">
               <label>ID</label>
-              <input type="text" class="form-control" name="equipo_id" id="equipo_id" placeholder="ID....">
+              <input type="text" class="form-control" name="id" id="id" placeholder="ID....">
             </div>
           </div>
          
           <div class="form-row">
             <div class="form-group col-md-6">
               <label for="inputCity">Nombre</label>
-              <input type="text" class="form-control" name="marca" id="marca">
+              <input type="text" class="form-control" name="nombre" id="nombre">
             </div>
             
             <div class="form-group col-md-6">
             <label for="selectMantenimiento">Cargo</label>
-            <select class="form-control" name="selectTipoEquipo" id="selectTipoEquipo">
+            <select class="form-control" name="cargo" id="cargo">
               <option>Selecciona una opcion</option>
-              <?php
-              foreach ($tipoEquipoResult as $tipoEquipoData) {
-              ?>
-                <option value=<?php echo $tipoEquipoData["id"] ?>><?php echo $tipoEquipoData["nombre"] ?></option>
-              <?php
-              }
-              ?>
+              <option value="mecanico">Mecanico</option>
+              <option value="soldador">Soldador</option>
+              <option value="maquinista">Maquinista</option>
             </select>
           </div>
           </div>
